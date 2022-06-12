4495fd32b037a15e353dce9c92438f5e90be5d1c
Enimofe <enimofeodujirin@gmail.com>
2022-06-12 13:59:36 +0100

    my first commit

---
+++ blog/forms.py
@@ -0,0 +1,66 @@
+import email
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blog.models import User
+from flask_login import current_user
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+             validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email Address',
+             validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Theres already an account with this username, please choose another.')
+    def validate_email(self, email):
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Theres already an account with this email, please choose another.')
+
+    def __repr__(self):
+        return self.email
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email Address',
+             validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+
+    def __repr__(self):
+        return self.email
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+             validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email Address',
+             validators=[DataRequired(), Email()])
+    profile_picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+   
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Theres already an account with this username, please choose another.')
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Theres already an account with this email, please choose another.')
+
+    def __repr__(self):
+        return self.email
