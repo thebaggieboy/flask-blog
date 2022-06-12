e5ff4456032e359e881309adf1f2107b8128a16b
Enimofe <enimofeodujirin@gmail.com>
2022-06-12 21:40:45 +0100

    f'

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,15 @@
 import email
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
 from flask_login import current_user
 
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,8 @@
 
 import os
-from flask import render_template, url_for, redirect, flash, request
+from flask import render_template, url_for, redirect, flash, request, abort
 from PIL import Image
-from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from blog.models import User, Post
 from blog import app, db, bcrypt
 from flask_login import login_required, login_user, current_user, logout_user, login_required
@@ -10,7 +10,9 @@ import secrets
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.title.desc()).paginate(per_page=4)
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -83,3 +85,57 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pic/' + current_user.image_file)
     return render_template('account.html', image_file=image_file, form=form, )
+
+
+
+@app.route('/new/post/', methods=['GET', 'POST'])
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+
+
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    
+    return render_template('new_post.html', form=form,  legend='New Post')
+
+@app.route('/post/<int:post_id>/', methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'info')
+        return redirect(url_for('post', post_id=post.id))
+
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('new_post.html', post=post, form=form, legend='Update Post')
+
+@app.route('/post<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'danger')
+    return redirect(url_for('home'))
+   
