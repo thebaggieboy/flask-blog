42b1e5a261b893bcfa127fdea9e797c2e7054e64
Enimofe <enimofeodujirin@gmail.com>
2022-06-15 18:48:09 +0100

    as

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '50d86ec193def31d8d172e2d2685f6dc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lwpvzjmgdtrjgb:22a188a7e6041a788bc732ce7d4c7dba606785de7b6f7796cf7a9274050db1e3@ec2-34-227-120-79.compute-1.amazonaws.com:5432/d9eoe35v7ucfdn'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
