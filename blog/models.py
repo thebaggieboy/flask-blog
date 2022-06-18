8f047b8d43a85ab37d1d99914f15fee1d5ce1f2e
Enimofe <enimofeodujirin@gmail.com>
2022-06-18 01:48:34 +0100

    Deploy

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,8 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
 
     image_file = db.Column(db.String(20), nullable=False, default='default.png')
     password = db.Column(db.String(60), nullable=False)
