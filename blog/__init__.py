107d1e30d48989a6bc83b9decf30e39067958286
Enimofe <enimofeodujirin@gmail.com>
2022-06-18 01:40:25 +0100

    Deploy

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '50d86ec193def31d8d172e2d2685f6dc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:// postgres://otkmfjuqnhzdzw:51fa9452c29b490f554b1ae1188e1a479b752308248ca7548aab5e001c9a3094@ec2-52-206-182-219.compute-1.amazonaws.com:5432/db0a9m697iqb0j'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://otkmfjuqnhzdzw:51fa9452c29b490f554b1ae1188e1a479b752308248ca7548aab5e001c9a3094@ec2-52-206-182-219.compute-1.amazonaws.com:5432/db0a9m697iqb0j'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
