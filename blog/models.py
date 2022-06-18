16f5b3bbe4d0a2ba54aeb1e25f1bb233679dd03c
Enimofe <enimofeodujirin@gmail.com>
2022-06-18 01:51:13 +0100

    Deploy

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(250), unique=True, nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
 
-    image_file = db.Column(db.String(20), nullable=False, default='default.png')
-    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(250), nullable=False, default='default.png')
+    password = db.Column(db.String(250), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
@@ -22,7 +22,7 @@ class User(db.Model, UserMixin):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.String(250), nullable=False)
     #date_posted = db.DateTime(db.String(20), nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
